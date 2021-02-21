37fd9ff5e640c9e04e743ba5a49ba4509f580097
Mariam-Essam <mariam.arabengineer@gmail.com>
2021-02-21 04:46:57 +0200

    ready-for-deploying

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(path.join(__dirname, '/dist/movies-app')));
+// app.get('/*', (req, res) =>{
+//     res.sendFile(path.join(`${__dirname}/dist/movies-app/index.html`))
+// });
+// app.listen(process.env.PORT || 3000);
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const fs = require('fs');
+
 const app = express();
-app.use(express.static(path.join(__dirname, '/dist/movies-app')));
-app.get('/*', (req, res) =>{
-    res.sendFile(path.join(`${__dirname}/dist/movies-app/index.html`))
+
+const port = process.env.PORT || 8000;
+
+const root = path.join(__dirname, 'dist', 'movies-app');
+app.get('*' ,function(req, res) {
+  fs.stat(root + req.path, function(err){
+    if(err){
+        res.sendFile("index.html", { root });
+    }else{
+        res.sendFile(req.path, { root });
+    }
+  })
 });
-app.listen(process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
