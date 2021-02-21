3154ffec51ae3cffe79eef6a7ad6d0928ffb5749
Mariam-Essam <mariam.arabengineer@gmail.com>
2021-02-21 04:15:40 +0200

    ready-for-deploying

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '/dist/movies-app')));
-app.get('/*', async (req, res) =>{
+app.get('/*', (req, res) =>{
     res.sendFile(path.join(`${__dirname}/dist/movies-app/index.html`))
 });
 app.listen(process.env.PORT || 3000);
