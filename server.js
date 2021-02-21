fee9854a5ee7e44b716c90ff2318e17323b3bf77
Mariam-Essam <mariam.arabengineer@gmail.com>
2021-02-21 04:12:28 +0200

    updated

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, '/dist/movies-app')));
 app.get('/*', async (req, res) =>{
-    res.sendFile('index.html',{root:path.join(`${__dirname}/dist/movies-app/index`)})
+    res.sendFile(path.join(`${__dirname}/dist/movies-app/index.html`))
 });
 app.listen(process.env.PORT || 3000);
